bfd4e09e82d5a9bff5c6fecdefacfcb1ab3e9bd7
Jan Jezek <mail@mediatainment-productions.com>
2016-05-30 22:48:47 +0200

    added constants

---
+++ Pokedexxx/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Pokedexxx
+//
+//  Created by Jan Jezek on 30.05.16.
+//  Copyright © 2016 mediatainment-productions. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
