6720b7816f4c2bc89a2ba1e28fde95026c8526ed
Jan Jezek <mail@mediatainment-productions.com>
2016-05-30 22:51:11 +0200

    added pokeurl in initializer

--- Pokedexxx/Pokemon.swift
+++ Pokedexxx/Pokemon.swift
@@ -20,11 +20,13 @@ class Pokemon {
     private var _weight: String!
     private var _attack: String!
     private var _nextEvolutionText: String!
+    private var _pokemonUrl: String!
     
     // getters
     var name: String {
         return _name
     }
+    
     
     
     var pokedexId: Int {
@@ -34,5 +36,7 @@ class Pokemon {
     init(name: String, pokedexId: Int) {
         self._name = name
         self._pokedexId = pokedexId
+        
+        _pokemonUrl = "\(URL_BASE)\(URL_POKEMON)\(self._pokedexId)"
     }
 }
