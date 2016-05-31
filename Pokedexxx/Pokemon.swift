23fa1e43ad44be75f6c473d7b70240692be90149
Jan Jezek <mail@mediatainment-productions.com>
2016-05-31 14:23:30 +0200

    implemented second api request and mapping to view

--- Pokedexxx/Pokemon.swift
+++ Pokedexxx/Pokemon.swift
@@ -21,15 +21,79 @@ class Pokemon {
     private var _weight: String!
     private var _attack: String!
     private var _nextEvolutionText: String!
+    private var _nextEvolutionId: String!
+    private var _nextEvolutionLevel: String!
     private var _pokemonUrl: String!
     
     // getters
     var name: String {
-        return _name
+      return _name
     }
     
     var pokedexId: Int {
         return _pokedexId
+    }
+    
+    var description: String {
+        if _description == nil {
+            _description = ""
+        }
+        return _description
+    }
+    
+    var type: String {
+        if _type == nil {
+            _type = ""
+        }
+        return _type
+    }
+    
+    var defense: String {
+        if _defense == nil {
+            _defense = ""
+        }
+        return _defense
+    }
+    
+    var height: String {
+        if _height == nil {
+            _height = ""
+        }
+        return _height
+    }
+    
+    var weight: String {
+        if _weight == nil {
+            _weight = ""
+        }
+        return _weight
+    }
+    var attack: String {
+        if _attack == nil {
+            _attack = ""
+        }
+        return _attack
+    }
+    
+    var nextEvolutionText: String {
+        if _nextEvolutionText == nil {
+            _nextEvolutionText = ""
+        }
+        return _nextEvolutionText
+    }
+    
+    var nextEvolutionId: String {
+        if _nextEvolutionId == nil {
+            _nextEvolutionId = ""
+        }
+        return _nextEvolutionId
+    }
+    
+    var nextEvolutionLevel: String {
+        if _nextEvolutionLevel == nil {
+            _nextEvolutionLevel = ""
+        }
+        return _nextEvolutionLevel
     }
     
     init(name: String, pokedexId: Int) {
@@ -98,10 +162,35 @@ class Pokemon {
                                         debugPrint(self._description)
                                     }
                                 }
+                                // make the callback, so the app can continoue
+                                completed()
                             }
                         }
                     } else {
                         self._description = ""
+                    }
+                }
+                
+                if let evolutions = dict["evolutions"] as? [Dictionary<String, AnyObject>] where evolutions.count > 0 {
+                    
+                    if let to = evolutions[0]["to"] as? String {
+                        
+                        // We do not support "mega" Pokemons
+                        if to.rangeOfString("mega") == nil {
+                            if let evo_uri = evolutions[0]["resource_uri"] as? String {
+                                let str_without_pokemon_url = evo_uri.stringByReplacingOccurrencesOfString("/api/v1/pokemon/", withString: "")
+                            
+                                let str_without_slash = str_without_pokemon_url.stringByReplacingOccurrencesOfString("/", withString: "")
+                                
+                                self._nextEvolutionId = str_without_slash
+                                self._nextEvolutionText = to
+                                
+                                if let lvl = evolutions[0]["level"] as? Int {
+                                    self._nextEvolutionLevel = "\(lvl)"
+                                }
+                                
+                            }
+                        }
                     }
                 }
             }
--- Pokedexxx/PokemonDetailVC.swift
+++ Pokedexxx/PokemonDetailVC.swift
@@ -11,19 +11,20 @@ import UIKit
 class PokemonDetailVC: UIViewController {
 
     var pokemon: Pokemon!
+    
     @IBOutlet weak var pokemonName: UILabel!
     @IBOutlet weak var pokemonImage: UIImageView!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
     @IBOutlet weak var typeLabel: UILabel!
     @IBOutlet weak var heightLabel: UILabel!
-    @IBOutlet weak var widthLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
     @IBOutlet weak var pokedexLabel: UILabel!
     @IBOutlet weak var attackLabel: UILabel!
     @IBOutlet weak var currentEvoImg: UIImageView!
     @IBOutlet weak var nextEvoImg: UIImageView!
     @IBOutlet weak var evoLabel: UILabel!
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -33,8 +34,30 @@ class PokemonDetailVC: UIViewController {
         pokemon.downloadPokemonDetails { () -> () in
             // this will be called after the download is done
             // the magic are the closures "() -> () in"
+            self.updateUI()
         }
-        
+    }
+    
+    func updateUI(){
+        descriptionLabel.text = pokemon.description
+        typeLabel.text = pokemon.type
+        heightLabel.text = pokemon.height
+        weightLabel.text = pokemon.weight
+        attackLabel.text = pokemon.attack
+        defenseLabel.text = pokemon.defense
+        currentEvoImg.image = UIImage(named: "\(pokemon.pokedexId)")
+        if pokemon.nextEvolutionId == "" {
+            evoLabel.text = "No Evolution"
+            nextEvoImg.hidden = true
+        } else {
+            nextEvoImg.hidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+            var str = "Next Evolution: \(pokemon.nextEvolutionText)"
+            
+            if pokemon.nextEvolutionLevel != "" {
+                str += " - LVL \(pokemon.nextEvolutionLevel)"
+            }
+        }
     }
 
     @IBAction func backButtonPressed(sender: AnyObject) {
