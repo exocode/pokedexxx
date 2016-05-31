d936e862388e0d74040c4a1eba76e9802c35f534
Jan Jezek <mail@mediatainment-productions.com>
2016-05-31 12:23:42 +0200

    added API request, mapped response to model

--- Pokedexxx/Pokemon.swift
+++ Pokedexxx/Pokemon.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Alamofire
 
 class Pokemon {
     
@@ -39,7 +40,71 @@ class Pokemon {
     }
     
     func downloadPokemonDetails(completed: DownloadComplete) {
-        // blsh
+       
+        let url = NSURL(string: _pokemonUrl)!
+        
+        // make a request to your API
+        Alamofire.request(.GET, url).responseJSON { response in
+            let result = response.result
+
+            if let dict = result.value as? Dictionary<String, AnyObject> {
+                
+                if let weight = dict["weight"] as? String {
+                    self._weight = weight
+                }
+                
+                if let height = dict["height"] as? String {
+                    self._height = height
+                }
+                
+                if let attack = dict["attack"] as? Int {
+                    self._attack = "\(attack)"
+                }
+                
+                if let defense = dict["defense"] as? Int {
+                    self._defense = "\(defense)"
+                }
+                
+                if let types = dict["types"] as? [Dictionary<String, String>] where types.count > 0 {
+                    
+                    if let type = types[0]["name"] {
+                        self._type = type
+                    }
+                    
+                    if types.count > 1 {
+                        
+                        // swift 3 style instead of 
+                        // var x = 1; x < types.count; x++
+                        for x in 1 ..< types.count {
+                            if let name = types[x]["name"] {
+                                self._type! += "/\(name)"
+                            }
+                        }
+                    } else {
+                        self._type = ""
+                    }
+                    
+                    if let descArr = dict["descriptions"] as? [Dictionary<String,String>] where descArr.count > 0 {
+                        if let url = descArr[0]["resource_uri"] {
+
+                            let nsurl = NSURL(string: "\(URL_BASE)\(url)")!
+                            Alamofire.request(.GET, nsurl).responseJSON { response in
+                                
+                                let descResult = response.result
+                                if let descDict = descResult.value as? Dictionary<String, AnyObject> {
+
+                                    if let description = descDict["description"] as? String {
+                                        self._description = description
+                                        debugPrint(self._description)
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        self._description = ""
+                    }
+                }
+            }
+        }
     }
-    
-  }
+}
